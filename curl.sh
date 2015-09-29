9d6bbfbd5b741a82f70c496aaf0b1e36935f792b
Brendan A R Sechter <bsechter@sennue.com>
2015-09-29 15:47:31 +0900

    Added user agent variable to curl script.

--- curl.sh
+++ curl.sh
@@ -1,42 +1,43 @@
 #!/bin/sh
 
 export API_HOST='http://localhost:8080'
-export CURL_OPTIONS='-sS'
+export CURL_OPTIONS='-sS -A'
+export CURL_USER_AGENT='user-agent'
 . secure_curl.sh
 
 export API_VERB="GET"
 export API_ENDPOINT=""
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ENDPOINT="version"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ENDPOINT="ping"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ENDPOINT="time"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="account"
 export API_HEADER="Content-Type: application/json"
 export API_DATA="bad-data"
 printf "\n$API_VERB /$API_ENDPOINT [bad data]\n"
-curl $CURL_OPTIONS -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="account"
 export API_HEADER="Content-Type: application/json"
 export API_DATA="{\"device_id\":\"$DEVICE_ID\", \"name\":\"$NAME\", \"email\":\"$EMAIL\", \"password\":\"$PASSWORD\"}"
 printf "\n$API_VERB /$API_ENDPOINT [may exist]\n"
-curl $CURL_OPTIONS -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="account"
@@ -44,54 +45,54 @@ export API_HEADER="Content-Type: application/json"
 export API_DATA_RANDOM_ID=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32`
 export API_DATA="{\"device_id\":\"FakeID:$API_DATA_RANDOM_ID\", \"name\":\"$API_DATA_RANDOM_ID\", \"email\":\"${API_DATA_RANDOM_ID}@email.com\", \"password\":\"$API_DATA_RANDOM_ID\"}"
 printf "\n$API_VERB /$API_ENDPOINT [random values]\n"
-curl $CURL_OPTIONS -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="auth"
 export API_HEADER="Content-Type: application/json"
 export API_DATA="{\"device_id\":\"$DEVICE_ID\", \"password\":\"bad-password\"}"
 printf "\n$API_VERB /$API_ENDPOINT [wrong password]\n"
-curl $CURL_OPTIONS -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="auth"
 export API_HEADER="Content-Type: application/json"
 export API_DATA="{\"device_id\":\"$DEVICE_ID\", \"password\":\"$PASSWORD\"}"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ENDPOINT="room"
 export API_AUTH_HEADER="Authorization: bad-token"
 printf "\n$API_VERB /$API_ENDPOINT [bad token]\n"
-curl $CURL_OPTIONS -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ENDPOINT="room"
 export API_AUTH_HEADER="Authorization: $TOKEN"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ITEM="x"
 export API_ENDPOINT="room/$API_ITEM"
 export API_AUTH_HEADER="Authorization: $TOKEN"
 printf "\n$API_VERB /$API_ENDPOINT [bad syntax]\n"
-curl $CURL_OPTIONS -D - -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -D - -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ITEM="-1"
 export API_ENDPOINT="room/$API_ITEM"
 export API_AUTH_HEADER="Authorization: $TOKEN"
 printf "\n$API_VERB /$API_ENDPOINT [nonexistent item]\n"
-curl $CURL_OPTIONS -D - -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -D - -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ITEM="1"
 export API_ENDPOINT="room/$API_ITEM"
 export API_AUTH_HEADER="Authorization: $TOKEN"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="room"
@@ -101,7 +102,7 @@ export API_DATA_NAME="Test Room"
 export API_DATA_DESCRIPTION="Test room description."
 export API_DATA="{ \"name\": \"$API_DATA_NAME\", \"description\": \"$API_DATA_DESCRIPTION\" }"
 printf "\n$API_VERB /$API_ENDPOINT [may exist]\n"
-curl $CURL_OPTIONS -H "$API_AUTH" -H "$API_AUTH_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH" -H "$API_AUTH_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="room"
@@ -111,14 +112,14 @@ export API_DATA_NAME=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -
 export API_DATA_DESCRIPTION=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 96`
 export API_DATA="{ \"name\": \"$API_DATA_NAME\", \"description\": \"$API_DATA_DESCRIPTION\" }"
 printf "\n$API_VERB /$API_ENDPOINT [random values]\n"
-curl $CURL_OPTIONS -H "$API_AUTH_HEADER" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH_HEADER" -H "$API_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="GET"
 export API_ITEM="1"
 export API_ENDPOINT="message/$API_ITEM"
 export API_AUTH_HEADER="Authorization: $TOKEN"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH_HEADER" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" | python -m json.tool #| less
 
 export API_VERB="POST"
 export API_ENDPOINT="message"
@@ -128,5 +129,5 @@ export API_DATA_ROOM_ID="1"
 export API_DATA_BODY="Test Message: Hello, World!"
 export API_DATA="{ \"room_id\": \"$API_DATA_ROOM_ID\", \"body\": \"$API_DATA_BODY\" }"
 printf "\n$API_VERB /$API_ENDPOINT\n"
-curl $CURL_OPTIONS -H "$API_AUTH" -H "$API_AUTH_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
+curl $CURL_OPTIONS "$CURL_USER_AGENT" -H "$API_AUTH" -H "$API_AUTH_HEADER" -d "$API_DATA" "$API_HOST/$API_ENDPOINT" -X "$API_VERB" #| python -m json.tool #| less
 
